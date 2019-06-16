6611b76e204b30bd7d774f0f174d7c338442aea0
esdrasferreira <esdras.ferreira@outlook.com>
2019-06-16 16:36:22 -0300

    story

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -7,26 +7,26 @@ class StoryBrain {
   List<Story> _storyData = [
     Story(
         storyTitle:
-            'Seu carro explodiu um pneu em uma estrada sinuosa no meio do nada, sem recepção de telefone celular. Você decide pedir carona. Uma caminhonete enferrujada ronca até parar perto de você. Um homem com um chapéu de abas largas e olhos sem alma abre a porta do passageiro e pergunta: "Precisa de uma carona, garoto?".',
+            'Seu carro furou um pneu em uma estrada sinuosa no meio do nada, sem sinal de celular. Você decide pedir carona. Uma caminhonete enferrujada ronca até parar perto de você. Um homem com um chapéu de abas largas e olhos sem alma abre a porta do passageiro e pergunta: "Precisa de uma carona, garoto?".',
         choice1: 'Eu vou entrar. Obrigado pela ajuda!',
         choice2: 'Melhor perguntar se ele é um assassino primeiro.'),
     Story(
-        storyTitle: 'Ele balança a cabeça devagar, sem ênfase na pergunta.',
+        storyTitle: 'Ele balança a cabeça devagar, concordando com pergunta.',
         choice1: 'Pelo menos ele é honesto. Eu vou subir',
         choice2: 'Espere, eu sei como trocar um pneu.'),
     Story(
         storyTitle:
-            'Quando você começa a dirigir, o estranho começa a falar sobre seu relacionamento com sua mãe. Ele fica mais irritado e irritado a cada minuto. Ele pede para você abrir o porta-luvas. Dentro de você encontra uma faca ensanguentada, dois dedos decepados e uma fita cassete de Elton John. Ele alcança o porta-luvas.',
-        choice1: 'Eu amo o Elton John! Entregue-lhe a fita cassete.',
+            'Quando você começa a dirigir, o estranho começa a falar sobre seu relacionamento com sua mãe. Ele fica mais irritado e irritado a cada minuto. Ele pede para você abrir o porta-luvas. Dentro dele você encontra uma faca ensanguentada, dois dedos decepados e uma fita cassete de Elton John. Ele alcança o porta-luvas.',
+        choice1: 'Eu amo o Elton John! Entregua-lhe a fita cassete.',
         choice2: 'É ele ou eu! Você pega a faca e apunhala-o.'),
     Story(
         storyTitle:
-            'O que? Tal policial fora! Você sabia que os acidentes de trânsito são a segunda principal causa de morte acidental para a maioria dos adultos?',
+            'O que? Tal policial fora! Você sabe que os acidentes de trânsito são a segunda principal causa de morte acidental para a maioria dos adultos.',
         choice1: 'Reiniciar',
         choice2: ''),
     Story(
         storyTitle:
-            'Enquanto você atravessa o guard-rail e se inclina para as pedras irregulares abaixo, você reflete sobre a dúbia sabedoria de esfaquear alguém enquanto eles estão dirigindo um carro em que você está..',
+            'Enquanto você atravessa o guard-rail e se inclina para as pedras irregulares abaixo, você reflete sobre a sabedoria de esfaquear alguém enquanto ele está dirigindo um carro em que você está..',
         choice1: 'Reiniciar',
         choice2: ''),
     Story(
